20388a382fc89c0a5cda3657b15bab0b8e8c00b3
WarriorOfCode <natalya.salivonchik@mail.ru>
2015-12-19 21:30:53 +0300

    added main page

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.engine('html', require('ejs').renderFile);
+
 app.get('/', function(req, res){
-  res.send('express');
+  res.render('index.html');
 });
 
 app.listen(3000);
