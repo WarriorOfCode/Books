1f969bd348929bd86589e1ea5f3ce40145019f10
WarriorOfCode <natalya.salivonchik@mail.ru>
2016-01-11 14:31:21 +0300

    add includes, link to user and pathname /user/login

--- modules/pages.js
+++ modules/pages.js
@@ -31,29 +31,43 @@ router.get('/setting', function(req, res){
 });
 
 router.get('/user/:id', function (req, res){
-	var selectSql = "SELECT Books.* FROM Books, books_users WHERE books_users.id_user = ? AND books_users.id_book = Books.id";
-	var selectFriendSql = "SELECT * FROM friends WHERE id_follower = ? AND id_following = ?";
-	if (req.params.id == null) res.redirect('/');
-	connection.query("SELECT * FROM Users WHERE id = ? LIMIT 1", req.params.id, function(err, rows){
-		if (err) throw err;
-		if (rows !== null && rows.length > 0){
-			connection.query(selectSql, req.params.id, function(err, rows1){
-				if (err) throw err;
-				connection.query(selectFriendSql, [req.session.id, req.params.id], function(err, rows2){
+	if (isFinite(req.params.id)) {
+		user(req.params.id);
+	} else {
+		connection.query("SELECT id FROM Users WHERE NickName = ?", req.params.id, function(err, rows3){
+			if (err) throw err;
+			if (rows3 !==null && rows3.length > 0){
+				user(rows3[0].id);
+			} else {
+				res.redirect('/');
+			}
+		});
+	};
+	function user (params) {
+		var selectSql = "SELECT Books.* FROM Books, books_users WHERE books_users.id_user = ? AND books_users.id_book = Books.id";
+		var selectFriendSql = "SELECT * FROM friends WHERE id_follower = ? AND id_following = ?";
+		if (params == null) res.redirect('/');
+		connection.query("SELECT * FROM Users WHERE id = ? LIMIT 1", params, function(err, rows){
+			if (err) throw err;
+			if (rows !== null && rows.length > 0){
+				connection.query(selectSql, params, function(err, rows1){
 					if (err) throw err;
-					res.render('user.html', {login: req.session.login, user: rows, books: rows1, flag: rows2});
+					connection.query(selectFriendSql, [req.session.id, params], function(err, rows2){
+						if (err) throw err;
+						res.render('user.html', {login: req.session.login, user: rows, books: rows1, flag: rows2});
+					});
 				});
-			});
-		} else {
-			res.redirect('/');
-		}
-	})
+			} else {
+				res.redirect('/');
+			}
+		});
+	}
 });
 
 router.get('/', function(req, res){
 	connection.query('SELECT * FROM Books', function(err, rows) {
 		if (err) throw err;
-		res.render('index.html', {data: rows, login: req.session.login});
+		res.render('index.html', {books: rows, login: req.session.login});
   });
 });
 
