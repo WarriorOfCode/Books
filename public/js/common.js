ced46a448c23e32efb23a178c03b7573c3b9e906
WarriorOfCode <natalya.salivonchik@mail.ru>
2016-03-11 23:59:57 +0300

    used locate

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,32 @@
 angular
-	.module('Books', ['ngAnimate', 'ui.bootstrap'])
+	.module('Books', ['ngAnimate', 'ui.bootstrap', 'pascalprecht.translate'])
 	.controller('LogoutCtrl', ['$scope', '$http', LogoutCtrl])
 	.controller('SearchCtrl', ['$scope', SearchCtrl])
-	.controller('OfferCtrl', ['$scope', '$http', OfferCtrl]);
+	.controller('OfferCtrl', ['$scope', '$http', '$translate', OfferCtrl])
+	.controller('localazeCtrl', ['$scope', '$translate', localazeCtrl])
+	.config(['$translateProvider', function ($translateProvider) {
+		$translateProvider
+			.translations('en', translations)
+			.preferredLanguage('en');
+}]);
+
+var translations = {
+	message:{
+		offer: 'Спасибо за предложение, книга скоро будет добавлена!'	
+	}
+};
+
+function localazeCtrl ($scope, $translate) {
+	$translate('HEADLINE').then(function (headline) {
+		$scope.headline = headline;
+	});
+	$translate('PARAGRAPH').then(function (paragraph) {
+		$scope.paragraph = paragraph;
+	});
+	$translate('NAMESPACE.PARAGRAPH').then(function (anotherOne) {
+		$scope.namespaced_paragraph = anotherOne;
+	});
+};
 
 function LogoutCtrl($scope, $http) {
 	$scope.send = function () {
@@ -24,14 +48,17 @@ function SearchCtrl($scope) {
 	};
 }
 
-function OfferCtrl($scope, $http) {
+function OfferCtrl($scope, $http, $translate) {
 
 	$scope.send = function(){
 		if ($scope.book){
 			$http.put('/api/offer', $scope.book)
 			.success(function(data){
-				$scope.message = data["message"];
+				//$scope.message = data["message"];
 				$scope.book= {};
+				 $translate('message.offer').then(function (data) {
+				      $scope.message = data;
+				    });
 			})
 			.error(function(data){
 				console.log(data);
